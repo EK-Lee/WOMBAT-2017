a4cfaa27c9d4523b73646ba6284de6b959da537a
Dianne Cook <visnut@gmail.com>
2017-05-24 14:13:14 +1000

    adding a small shiny app

---
+++ pcp-mds/app.R
@@ -0,0 +1,109 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tourr)
+library(tidyverse)
+library(plotly)
+
+data(olive)
+olive_mds <- cmdscale(dist(apply(olive[,3:10], 2, scale)), 
+                      k=2, x.ret=TRUE)
+olive$region <- factor(olive$region, levels=1:3, labels=c("South", "Sardinia", "North"))
+olive <- olive %>% mutate(MDS1=olive_mds$points[,1],
+                          MDS2=olive_mds$points[,2])
+myscale <- function(x) (x - mean(x)) / sd(x)
+scale.dat.melt <- olive %>%
+  select(region, palmitic:eicosenoic) %>% 
+  mutate_each(funs(myscale), -region) %>%  
+  mutate(ids = 1:nrow(olive)) %>% 
+  gather(var, Value, -region, -ids, convert=TRUE) %>%
+  mutate(Variables = as.numeric(as.factor(var)))
+colnames(scale.dat.melt)[1] <- "Class"
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Parallel coordinate plot linked to MDS plot"),
+   fluidRow(column(
+     width = 11,
+     plotlyOutput("parallel", height = 400)
+   ), column(
+     width = 4, align = "center", offset = 2,
+     plotlyOutput("mdsplot", height = 400)
+   ))
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+  #Define reactive values for MDS plot and vote matrix plots
+  rv <- reactiveValues(data = data.frame(
+    MDS1 = olive_mds$points[,1], MDS2 = olive_mds$points[,2],
+    Class = olive[,1], ids = 1:nrow(olive),
+    fill = logical(nrow(olive))))
+  
+  #function to update selected elements in each plot
+  updateRV <- function(selected) {
+    fill <- logical(length(rv$data$fill))
+    fill[selected] <- TRUE
+    rv$data$fill <- fill
+  }
+  
+  output$parallel <- renderPlotly({
+    yy <- rv$data$ids[rv$data$fill]
+    
+      p <- ggplot(scale.dat.melt, aes(x = Variables, y = Value,
+                                      group = ids, key = ids, colour = Class, var = var)) +
+        geom_line(alpha = 0.3) + scale_x_discrete(limits = levels(as.factor(scale.dat.melt$var)), expand = c(0.01,0.01)) +
+        ggtitle("Data parallel plot ") + theme(legend.position = "none", axis.text.x  = element_text(angle = 90, vjust = 0.5), aspect.ratio = 1) +
+        scale_colour_brewer(type = "qual", palette = "Dark2")
+      
+      if (length(yy) > 0) {
+        dat <-   scale.dat.melt %>% dplyr::filter(ids %in% yy)
+        p <- ggplot(scale.dat.melt, aes(x = Variables, y = Value, 
+                                        group = ids, key = ids, color = Class, var = var)) +
+          geom_line(alpha = 0.1) + scale_x_discrete(limits = levels(as.factor(scale.dat.melt$var)), expand = c(0.01,0.01)) + 
+          ggtitle("Data parallel plot") + theme(legend.position = "none",axis.text.x  = element_text(angle = 90, vjust = 0.5), aspect.ratio = 1) + 
+          scale_colour_brewer(type = "qual",palette = "Dark2")
+        
+        p <- p + geom_line(data = dat) 
+      }
+    ggplotly(p,tooltip = c("var","colour","y","key")) %>% layout(dragmode = "select")
+  })
+  
+  #MDS plot
+  output$mdsplot <- renderPlotly({
+    yy <- rv$data$ids[rv$data$fill]
+    
+    p <- ggplot(data = rv$data, aes(x = MDS1, y = MDS2, 
+                                    colour = Class, key = ids)) + 
+      geom_point(size = I(3),alpha = .5)  + theme(legend.position = "none", legend.text = element_text(angle = 90), legend.key = element_blank(), aspect.ratio =
+                                                    1)  + labs(y = "MDS 2", x = "MDS 1", title = "Multidimensional Scaling") +
+      scale_colour_brewer(type = "qual",palette = "Dark2")
+    
+    if (length(yy) > 0) {
+      dat <- rv$data %>% dplyr::filter(ids %in% yy)
+      p <- ggplot(data = rv$data, aes(x = MDS1, y = MDS2, color = Class, key = ids)) + 
+        geom_point( size = I(3), alpha = .1) + theme(legend.position = "none", legend.text = element_text(angle = 90), legend.key = element_blank(), aspect.ratio =1) +
+        labs(y = "MDS 2", x = "MDS 1", title = "Multidimensional Scaling")  + 
+        scale_colour_brewer(type =   "qual",palette = "Dark2")
+      
+      p <- p + geom_point(data = dat, size =  I(3))
+      
+    }
+    ggplotly(p,tooltip = c("colour","x","y","key")) %>% layout(dragmode = "select")
+    
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
